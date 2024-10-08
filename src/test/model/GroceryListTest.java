3309ceccf6881a43bf768663103ba2239d64b51b
Nancy <yma60@student.ubc.ca>
2024-10-08 15:42:19 -0700

    Updated GroceryListTest class in testRemoveGrocery(), testCalcPercent()

--- src/test/model/GroceryListTest.java
+++ src/test/model/GroceryListTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import ui.Main;
+
 public class GroceryListTest {
     private GroceryList groceryListTest;
     private Grocery grocery1;
@@ -44,11 +46,11 @@ public class GroceryListTest {
         assertEquals("bread", firstGrocery.getName());
         assertEquals(5.00, firstGrocery.getPrice());
         assertEquals("grains", firstGrocery.getCategory());
+        assertEquals(0, groceryListTest.getOthersCount());
         assertEquals(1, groceryListTest.getGrainsCount());
         assertEquals(0, groceryListTest.getVegeCount());
         assertEquals(0, groceryListTest.getFruitCount());
         assertEquals(0, groceryListTest.getDairyCount());
-        assertEquals(0, groceryListTest.getOthersCount());
         assertEquals(5.00, groceryListTest.getTotalPrice());
 
         groceryListTest.addGrocery(grocery2);
@@ -88,21 +90,20 @@ public class GroceryListTest {
         assertEquals(0, groceryListTest.getOthersCount());
         assertEquals(6.00, groceryListTest.getTotalPrice());
 
-        groceryListTest.addGrocery(grocery1);
-        groceryListTest.addGrocery(grocery2);
         groceryListTest.removeGrocery(grocery2);
-        assertEquals(1, groceryListTest.getGroceries().size());
-        assertEquals(1, groceryListTest.getGrainsCount());
+        assertEquals(0, groceryListTest.getGroceries().size());
+        assertEquals(0, groceryListTest.getGrainsCount());
         assertEquals(0, groceryListTest.getVegeCount());
         assertEquals(0, groceryListTest.getFruitCount());
         assertEquals(0, groceryListTest.getDairyCount());
         assertEquals(0, groceryListTest.getOthersCount());
-        assertEquals(5.00, groceryListTest.getTotalPrice());
+        assertEquals(0.00, groceryListTest.getTotalPrice());
     }
 
     @Test 
     void testCalcPercent() {
         groceryListTest.addGrocery(grocery1);
+        groceryListTest.calcPercent();
         assertEquals(100.0, groceryListTest.getGrainsPerct());
         assertEquals(0.0, groceryListTest.getVegePerct());
         assertEquals(0.0, groceryListTest.getFruitPerct());
@@ -116,10 +117,13 @@ public class GroceryListTest {
         groceryListTest.addGrocery(grocery2);
         Grocery grocery3 = new Grocery("milk", 7.00, "dairy products");
         groceryListTest.addGrocery(grocery3);
-        assertEquals(33.33, groceryListTest.getGrainsPerct());
+
+        groceryListTest.calcPercent();
+       
+        assertEquals(33.0, groceryListTest.getGrainsPerct());
         assertEquals(0.0, groceryListTest.getVegePerct());
-        assertEquals(33.33, groceryListTest.getFruitPerct());
-        assertEquals(33.33, groceryListTest.getDairyPerct());
+        assertEquals(33.0, groceryListTest.getFruitPerct());
+        assertEquals(33.0, groceryListTest.getDairyPerct());
         assertEquals(0.0, groceryListTest.getOthersPerct());
     }
 }
