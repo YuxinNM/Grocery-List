346b9a3421779d5aa373a99d122232715baf05e1
Nancy <yma60@student.ubc.ca>
2024-10-21 12:53:24 -0700

    changed variable name

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -16,7 +16,7 @@ public class JsonReader {
         
     }
 
-    // EFFECTS: reads GroceryList (gl) from file and returns it;
+    // EFFECTS: reads GroceryList (groceryList) from file and returns it;
     //          throws IOException if an error occurs reading data from file
     public GroceryList read() throws IOException {
         return null;
@@ -33,16 +33,16 @@ public class JsonReader {
         return null;
     }
 
-    // MODIFIES: gl
+    // MODIFIES: groceryList
     // EFFECTS: helper for parseWorkRoom, parses Grocery objects from JSON object
     //          and adds them to GroceryList
-    private void addGroceries(GroceryList gl, JSONObject jsonObject) {
+    private void addGroceries(GroceryList groceryList, JSONObject jsonObject) {
 
     }
 
-    // MODIFIES: gl
+    // MODIFIES: groceryList
     // EFFECTS: helper for addGrocery, parses Grocery from JSON object and adds it to GroceryList
-    private void addGrocery(GroceryList gl, JSONObject jsonObject) {
+    private void addGrocery(GroceryList groceryList, JSONObject jsonObject) {
 
     }
 }
--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -1,8 +1,52 @@
 package persistence;
 
+import org.junit.Test;
+
+import model.GroceryList;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 // Referenced from the JsonSerialization Demo
 // https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
 
 public class JsonReaderTest extends JsonTest {
-    
+    @Test
+    void testReaderNonExistentFile() {
+        JsonReader reader = new JsonReader("./data/noSuchFile.json");
+        try {
+            GroceryList groceryList = reader.read();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    void testReaderEmptyWorkRoom() {
+        JsonReader reader = new JsonReader("./data/testReaderEmptyWorkRoom.json");
+        try {
+            WorkRoom wr = reader.read();
+            assertEquals("My work room", wr.getName());
+            assertEquals(0, wr.numThingies());
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+    @Test
+    void testReaderGeneralWorkRoom() {
+        JsonReader reader = new JsonReader("./data/testReaderGeneralWorkRoom.json");
+        try {
+            WorkRoom wr = reader.read();
+            assertEquals("My work room", wr.getName());
+            List<Thingy> thingies = wr.getThingies();
+            assertEquals(2, thingies.size());
+            checkThingy("needle", Category.STITCHING, thingies.get(0));
+            checkThingy("saw", Category.WOODWORK, thingies.get(1));
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
 }
