1eca3152db8db5579d2aff0201140da19301285a
Nancy <yma60@student.ubc.ca>
2024-10-21 13:23:25 -0700

    changed JsonTest back

--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -7,15 +7,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import model.Grocery;
 
 public class JsonTest {
-    protected void checkGroceryName(String name, Grocery grocery) {
+    protected void checkGroceryName(String name, double price, String category, Grocery grocery) {
         assertEquals(name, grocery.getName());
-    }
-
-    protected void checkGroceryPrice(double price, Grocery grocery) {
         assertEquals(price, grocery.getPrice());
-    }
-
-    protected void checkGroceryCategory(String category, Grocery grocery) {
         assertEquals(category, grocery.getCategory());
     }
 }
