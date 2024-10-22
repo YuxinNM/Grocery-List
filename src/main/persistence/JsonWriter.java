19987047e87227457e5e827bb58773de106c63d2
Nancy <yma60@student.ubc.ca>
2024-10-21 22:34:39 -0700

    added Writable interface

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,30 +1,36 @@
 package persistence;
 
-import java.io.FileNotFoundException;
-
 import model.GroceryList;
+
+import org.json.JSONObject;
+
+import java.io.*;
 
 // Referenced from the JsonSerialization Demo
 // https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
 
 // Represents a writer that writes JSON representation of groceryList to file
 public class JsonWriter {
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
 
     // EFFECTS: constructs writer to write to destination file
     public JsonWriter(String destination) {
-
+        this.destination = destination;
     }
 
     // MODIFIES: this
     // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot be found
     public void open() throws FileNotFoundException {
-
-    }
+        writer = new PrintWriter(new File(destination));
+    }   
 
     // MODIFIES: this
     // EFFECTS: writes JSON representation of groceryList to file
     public void write(GroceryList groceryList) {
-
+        JSONObject json = groceryList.toJason();
+        saveToFile(json.toString(TAB)); 
     }
 
     // MODIFIES: this
