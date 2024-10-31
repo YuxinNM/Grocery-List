558493681ac6929584f332b2cdaffd55b30d469c
Nancy <yma60@student.ubc.ca>
2024-10-31 13:57:22 -0700

    Added class level documentation

--- src/main/persistence/Writable.java
+++ src/main/persistence/Writable.java
@@ -2,6 +2,7 @@ package persistence;
 
 import org.json.JSONObject;
 
+// An interface that returns this as JSON object
 public interface Writable {
     // EFFECTS: returns this as JSON object
     JSONObject toJson();
