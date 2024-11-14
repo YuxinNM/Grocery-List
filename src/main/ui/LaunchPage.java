8aaebec1a73d85663a38620c8f9dd55dd4cb46bd
Nancy <yma60@student.ubc.ca>
2024-11-13 20:25:34 -0800

    Implemented actionPerformed()

--- src/main/ui/LaunchPage.java
+++ src/main/ui/LaunchPage.java
@@ -1,21 +1,30 @@
 
 package ui;
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
+import model.GroceryList;
+import persistence.JsonReader;
+
 // A launch window that asks user to laod saved file
 public class LaunchPage implements ActionListener{
     JFrame loadFileFrame = new JFrame();
-    JButton loadButton = new JButton();
-    JButton notLoadButton = new JButton();
+    JButton loadButton = new JButton("Load Previous Grocery List");
+    JButton notLoadButton = new JButton("Not Load Previous Grocery List");
+    private static final String JSON_DESTINATION = "./data/grocerylist.json";
+    private GroceryList groceryList;
+    JsonReader jsonReader;
 
     // EFFECTS: a window that asks user to choose one of two buttons to load or 
     //          not load saved grocery list, leads to a new window with the given choice
     public LaunchPage() {
+        jsonReader = new JsonReader(JSON_DESTINATION);
         setUpFrame();
         setUpButton(loadButton);
         setUpButton(notLoadButton);
@@ -27,13 +36,13 @@ public class LaunchPage implements ActionListener{
     public void setUpFrame() {
         loadFileFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         loadFileFrame.setSize(500,500);
-        loadFileFrame.setLayout(null);
+        loadFileFrame.setLayout(new GridLayout(2, 1, 5, 5));
         loadFileFrame.setVisible(true);
     }
 
     // EFFECTS: sets up a JButton
     public void setUpButton(JButton button) {
-        button.setBounds(150, 150, 200, 50);
+        button.setSize(200, 250);
         button.setFocusable(false);
         button.addActionListener(this);
     }
@@ -42,6 +51,21 @@ public class LaunchPage implements ActionListener{
     //          with loaded or empty groceryList indicated by the button user clicks
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if (e.getSource() == loadButton) {
+            loadFileFrame.dispose();
+            try {
+                groceryList = jsonReader.read();
+                new GraphicalGroceryApp(groceryList);
+            } catch (IOException readerror) {
+                JFrame errorFrame = new JFrame();
+                JButton errorButton = new JButton("Unable to read from file" + JSON_DESTINATION);
+                setUpButton(errorButton);
+                errorFrame.add(errorButton);
+            }
+        } 
+        if (e.getSource() == notLoadButton) {
+            loadFileFrame.dispose();
+            new GraphicalGroceryApp(new GroceryList());
+        }
     }
 }
