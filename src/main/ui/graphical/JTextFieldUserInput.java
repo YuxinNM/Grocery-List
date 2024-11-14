9c3410f13b355eabb1129bf0e1ffb9aed5dbf74d
Nancy <yma60@student.ubc.ca>
2024-11-14 15:24:06 -0800

    implemented the JTextFIeldUserInpu constructor

--- src/main/ui/graphical/JTextFieldUserInput.java
+++ src/main/ui/graphical/JTextFieldUserInput.java
@@ -1,11 +1,20 @@
 package ui.graphical;
 
+import java.awt.Font;
+import java.awt.Insets;
+
 import javax.swing.JTextField;
 
 // sets up JTextFields that can be used to take user input
 public class JTextFieldUserInput extends JTextField {
+    private static final Font textFont = new Font("Arial", Font.BOLD, 15);
+    private static final Insets textMargins = new Insets(5, 10, 5, 10);
 
-    // EFFECTS: sets the textbox size as desired, font
+    // EFFECTS: sets the textbox size as desired, font, tool tip text, margins
     public JTextFieldUserInput(int size) {
+        super(size);
+        this.setFont(textFont);
+        this.setToolTipText("Enter Your Input Here");
+        this.setMargin(textMargins);
     }
 }
