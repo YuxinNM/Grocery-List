c02d7a3ec263c804031f1f57464ed6052cd78fd5
Nancy <yma60@student.ubc.ca>
2024-11-14 15:17:32 -0800

    Added JTextFieldUserInput class specification

---
+++ src/main/ui/graphical/JTextFieldUserInput.java
@@ -0,0 +1,11 @@
+package ui.graphical;
+
+import javax.swing.JTextField;
+
+// sets up JTextFields that can be used to take user input
+public class JTextFieldUserInput extends JTextField {
+
+    // EFFECTS: sets the textbox size as desired, font
+    public JTextFieldUserInput(int size) {
+    }
+}
