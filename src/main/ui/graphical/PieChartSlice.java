0aba6ef6d916217860d601783f4478000418b4f3
Nancy <yma60@student.ubc.ca>
2024-11-20 20:38:00 -0800

    specification for PieChartSlice()

--- src/main/ui/graphical/PieChartSlice.java
+++ src/main/ui/graphical/PieChartSlice.java
@@ -4,5 +4,11 @@ import java.awt.Color;
 
 // Represents a slice in the pie chart
 public class PieChartSlice {
+    private double sliceSize;
+    private Color sliceColor;
 
+    //EFFECTS: creates a slice in the pie chart with given size and color
+    public PieChartSlice(double size, Color color) {
+        
+    }
 }
