366b44fafdab65c421430add3e02097a99d322da
Nancy <yma60@student.ubc.ca>
2024-11-20 20:35:19 -0800

    Created two classes for pie chart with class-level comments

---
+++ src/main/ui/graphical/PieChart.java
@@ -0,0 +1,9 @@
+package ui.graphical;
+
+import java.awt.*;
+import javax.swing.*;
+
+// Creates a piechart with given slices
+public class PieChart {
+
+}
---
+++ src/main/ui/graphical/PieChartSlice.java
@@ -0,0 +1,8 @@
+package ui.graphical;
+
+import java.awt.Color;
+
+// Represents a slice in the pie chart
+public class PieChartSlice {
+
+}
