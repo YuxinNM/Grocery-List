59bc415ceaf46005bb89be9549cb4d7f88b95ac2
Nancy <yma60@student.ubc.ca>
2024-11-26 13:28:59 -0800

    Created interface LogPrinter and class LogException

---
+++ src/main/model/exception/LogException.java
@@ -0,0 +1,16 @@
+package model.exception;
+
+// reference:https://github.students.cs.ubc.ca/CPSC210/AlarmSystem
+/**
+ * Represents the exception that can occur when
+ * printing the event log.
+ */
+public class LogException extends Exception {
+    public LogException() {
+		super("Error printing log");
+	}
+	
+	public LogException(String msg) {
+		super(msg);
+	}
+}
---
+++ src/main/ui/LogPrinter.java
@@ -0,0 +1,11 @@
+package ui;
+
+import model.EventLog;
+import model.exception.LogException;
+
+// reference: https://github.students.cs.ubc.ca/CPSC210/AlarmSystem
+// Defines behaviours that event log printers must support
+public interface LogPrinter {
+    // prints the log given, throws LogException when printing fails 
+    void printLog(EventLog el) throws LogException;
+}
