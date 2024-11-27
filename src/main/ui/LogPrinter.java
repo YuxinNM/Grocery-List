b2c1fefdcbcdfaabcb5abca7abba60b7017e1e4e
Nancy <yma60@student.ubc.ca>
2024-11-27 12:40:00 -0800

    added a title for specification

--- src/main/ui/LogPrinter.java
+++ src/main/ui/LogPrinter.java
@@ -6,6 +6,6 @@ import model.exception.LogException;
 // reference: https://github.students.cs.ubc.ca/CPSC210/AlarmSystem
 // Defines behaviours that event log printers must support
 public interface LogPrinter {
-    // prints the log given, throws LogException when printing fails 
+    // EFFECTS: prints the log given, throws LogException when printing fails 
     void printLog(EventLog el) throws LogException;
 }
