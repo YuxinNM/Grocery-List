c13437ef72f850c6062cbe570e5e78c156677fd3
Nancy <yma60@student.ubc.ca>
2024-11-27 12:39:08 -0800

    Added printEventLog, fixed checkstyle

---
+++ src/main/ui/printEventLog.java
@@ -0,0 +1,23 @@
+package ui;
+
+import model.Event;
+
+import model.EventLog;
+import model.exception.LogException;
+
+// Prints out the events logged
+public class printEventLog implements LogPrinter {
+    
+    //EFFECT: Constructor sets up the printer to the console
+    public printEventLog() {
+        super();
+    }
+ 
+    // EFFECT: prints all the events in the eventlog to the console
+    @Override
+    public void printLog(EventLog el) throws LogException {
+        for (Event next : el) {
+            System.out.println(next.toString());
+        }
+    }
+}
